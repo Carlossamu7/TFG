78d4d30399d813740b0ee3790f05a72e3bf0044e
Carlos Sánchez <carlossamu7@gmail.com>
2020-03-09 16:51:36 +0100

    Comencemos

---
+++ tfg.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 17:54:38 2020
+@author: Carlos Sánchez Muñoz
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+
+#######################
+###   EJERCICIO 1   ###
+#######################
+
+""" Ejecución  """
+def ejercicio1():
+	return 0
+
+#######################
+###       MAIN      ###
+#######################
+
+""" Programa principal. """
+def main():
+    x=1
+
+if __name__ == "__main__":
+    main()
