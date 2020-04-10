af0b33bfea612565aae5dbe011719656b4be6325
Carlos Sánchez <carlossamu7@gmail.com>
2020-04-11 01:32:11 +0200

    a por el threesholding

--- tfg.py
+++ tfg.py
@@ -4,12 +4,10 @@ Created on Mon Feb 17 17:54:38 2020
 @author: Carlos Sánchez Muñoz
 """
 
-import numpy as np
 from matplotlib import pyplot as plt
+import numpy as np
 import math
-
 import cv2
-from PIL import Image
 
 ###########################################
 ###   LECTURA E IMPRESIÓN DE IMÁGENES   ###
@@ -82,22 +80,52 @@ def pintaI(image, flag_color=1, image_title = "Imagen", window_title = "Ejercici
     plt.show()
     image = image.astype(np.float64)    # Devolvemos su formato
 
-#######################
-###   FUNCIONES   ###
-#######################
+""" Lee una lista de imágenes ya sea en grises o en color. Devuelve la lista de imágenes leída.
+- image_list: lista de imágenes a concatenar.
+- flag_color (op): modo en el que se van a leer las imágenes. Por defecto en color.
+"""
+def leer_lista_imagenes(file_name_list, flag_color = 1):
+    image_list = []
 
-def show_images(images, titles):
-# Display all the images
+    for i in file_name_list:
+        img = leer_imagen(i, flag_color)
+        image_list.append(img)
+
+    return image_list
+
+""" Muestra múltiples imágenes en una ventena Matplotlib.
+- img_list: La lista de imágenes.
+- title_list: Lista de títulos de las imágenes.
+- flag_color (op): bandera para indicar si la imagen es en B/N o color. Por defecto color.
+- window_title (op): título de la ventana. Por defecto 'Imágenes con títulos'
+"""
+def show_images(img_list, title_list, flag_color=0, window_title="Haar Wavelets"):
     fig = plt.figure(figsize=(12, 6))
-    for i, a in enumerate(images):
+    fig.canvas.set_window_title(window_title)
+
+    for i in range(len(img_list)):
+        normaliza(img_list[i], title_list[i])
+        img_list[i] = img_list[i].astype(np.uint8)
+
+    for i, a in enumerate(img_list):
         display = fig.add_subplot(1, 3, i + 1) #(1, 4, i + 1)
-        display.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
-        display.set_title(titles[i], fontsize=10)
-        display.set_xticks([])
-        display.set_yticks([])
+        if flag_color == 0:
+            display.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
+        else:
+            display.imshow(a, interpolation="nearest")
+        display.set_title(title_list[i], fontsize=10)
+        display.set_xticks([])          # Deshabilitamos xticks
+        display.set_yticks([])          # e y_ticks
 
     fig.tight_layout()
     plt.show()
+
+    for i in range(len(img_list)):
+        img_list[i] = img_list[i].astype(np.float64)    # lo devolvemos a su formato.
+
+#######################
+###   FUNCIONES   ###
+#######################
 
 """ Calcula la transformada de Haar de una lista. Devuelve la transformada.
 - list: lista sobre la que realizar el cálculo.
@@ -115,18 +143,17 @@ def haar_transform(list):
 
 """ Realiza el algoritmo Haar Wavelet de manera recursiva realizando las particiones
 adecuadas y usando la función 'haar_transform'. Devuelve una lista con la compresión.
-- list:
-- length:
-- offset:
+- list: lista a la que hacer el split de haar.
+- offset: parte fija del split.
 """
-def haar_split(list, length, offset):
-	if( length > 2 ):
+def haar_split(list, offset):
+	if(len(list) > 2):
 		first = list[:len(list)//2]
 		second = list[len(list)//2:]
 
 		first = haar_transform(first)
 		offset = second + offset
-		return haar_split(first, length/2, offset)
+		return haar_split(first, offset)
 
 	else:
 		return list + offset
@@ -138,23 +165,23 @@ def haar_row(img):
 	haar_row = []
 	for pixels in img:
 		haar = haar_transform(pixels)
-		haar_row.append(haar_split(haar, len(haar), []))
+		haar_row.append(haar_split(haar, []))
 	return haar_row
 
 """ Calcula la transformada de Haar una imágen.
 - img: imagen original a transformar.
 """
 def haar_image(img, img_title):
-	print("Calculando la transformada de Haar a la imagen " + img_title +".")
+	print("Calculando la transformada de Haar a la imagen '" + img_title +"'.")
 	by_row = haar_row(img)			# por filas
 	transpose = zip(*by_row)		# transponemos
 	haar_img = haar_row(transpose)	# por columnas
 	return haar_img
 
 """ Calcula la transformada inversa de Haar. Devuelve la lista resultante.
-- front:
-- the_rest:
-- power:
+- front: primera parte de la lista.
+- the_rest: segunda parte de la lista.
+- power: 2 elevadoa este exponente me dice el índice de the_rest en la lista.
 """
 def reverse_haar(front, the_rest, power):
 	reverse = []
@@ -175,7 +202,7 @@ Devuelve la imagen original.
 - haar_img: imagen después de la transformada de Haar.
 """
 def reverse_image(haar_img, img_title):
-	print("Restaurando la imagen original de la transformada de Haar de " + img_title + ".")
+	print("Restaurando la imagen original de la transformada de Haar de '" + img_title + "'.")
 	rev_columns = []
 	power = 0
 
@@ -198,24 +225,26 @@ def reverse_image(haar_img, img_title):
 
 """ Programa principal. """
 def main():
-	#img_title = input("Image name: ")
-	#img_title = "BigBoiLion.jpg"
-	img_title = "lena512.bmp"
+    #img_title = input("Image name: ")
+    img_title = "BigBoiLion.jpg"
+    #img_title = "lena512.bmp"
 
-	img = leer_imagen("images/" + img_title, 0)
-	print("La imagen {} tiene tamaño {}x{}.".format(img_title, len(img), len(img[0])))
+    img = leer_imagen("images/" + img_title, 0)
+    print("La imagen '{}' tiene tamaño {}x{}.".format(img_title, len(img), len(img[0])))
 
-	haar_img = np.array(haar_image(img, img_title))
-	rev_img = np.array(reverse_image(haar_img, img_title))
+    haar_img = np.array(haar_image(img, img_title))
+    rev_img = np.array(reverse_image(haar_img, img_title))
 
-	if (True):
-		print("\nMatriz de la imagen original:")
-		print(img)
-		print("\nMatriz de la imagen restaurada:")
-		print(rev_img)
+    if (True):
+        print("\nMatriz de la imagen original:")
+        print(img)
+        print("\nMatriz después de la transformada de Haar:")
+        print(haar_img)
+        print("\nMatriz de la imagen restaurada:")
+        print(rev_img)
 
-	show_images([img, haar_img, rev_img],
-				titles = ["Original", "2D Haar Transform", "Return to Original"])
+    show_images([img, haar_img, rev_img],
+                ["Original", "2D Haar Transform", "Return to Original"])
 
 if __name__ == "__main__":
 	main()
